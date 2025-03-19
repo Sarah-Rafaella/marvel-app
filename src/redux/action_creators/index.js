b96fbb4d5e876c96f5216ca64b48be2b9fecd4ee
Sarah-Rafaella <seu.email@example.com>
2025-03-19 20:29:19 -0300

    Nova branch

--- src/redux/action_creators/index.js
+++ src/redux/action_creators/index.js
@@ -19,11 +19,11 @@ import {
 } from "../actions";
 import md5 from "js-md5";
 
-const PUBLIC_KEY = "90f3c7ccb35efbb76e4bc98668685d38";
+const PUBLIC_KEY = "453e60dfe99509e2b7d18976b9195cf9";
 const TIME_STAMP = Date.now().toString();
 
 const hashGenerator = () => {
-  const PRIVATE_KEY = "fd9435ff14b92b7935b7cb39495860414f142707";
+  const PRIVATE_KEY = "9e1e04557eb3e26e96fc6cb9757439f41c283d58";
   const HASH = md5.create();
   HASH.update(TIME_STAMP + PRIVATE_KEY + PUBLIC_KEY);
   return HASH.hex();
